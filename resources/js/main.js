1328a06b37049ca4f76094b9e9ded47fe615460e
lawdy33 <lawdy33@gmail.com>
2018-01-19 21:18:16 -0800

    updated homepage

---
+++ resources/js/main.js
@@ -0,0 +1,18 @@
+// Slick - Carousel - jumbotron photos //
+
+$(document).ready(function () {
+
+  $('.glasses').slick( {
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 900,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    rtl: true,
+    speed: 1500,
+    easing: easein
+  });
+
+
+});
